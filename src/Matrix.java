ddb58c7cc541665c4e2e7db0681485cd66d83947
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 14:27:40 +0300

    fields and constructors

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,5 +2,29 @@
  * Created by Dimandus on 07.11.2015.
  */
 public class Matrix {
-    
+
+    double[][] items;
+    int rowCount;
+    int columnCount;
+
+    public Matrix(double[][] source) {
+        items = source;
+        if(source!=null) {
+            rowCount = source.length;
+            columnCount = source[0].length;
+        }
+        else
+        {
+            rowCount = 0;
+            columnCount =0;
+        }
+    }
+
+    public Matrix(int rowCount, int columnCount) {
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+
+        items = new double[rowCount][columnCount];
+
+    }
 }
