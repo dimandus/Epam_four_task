28fcfac9f9507292b917a15513f1d81d800e4f56
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 14:58:02 +0300

    new test directory and template tests for arrays

---
+++ tests/ArrayToolsTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dimandus on 07.11.2015.
+ */
+public class ArrayToolsTest {
+
+    @Test
+    public void testChangeLength() throws Exception {
+
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+
+    }
+
+    @Test
+    public void testShuffle() throws Exception {
+
+    }
+
+    @Test
+    public void testPrint() throws Exception {
+
+    }
+}
