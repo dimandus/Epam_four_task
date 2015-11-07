0bff3952a1d17ef6be53a01984f3aac7724c7c43
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 14:08:49 +0300

    new compare function

--- src/ArrayTools.java
+++ src/ArrayTools.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by Dimandus on 07.11.2015.
  */
@@ -12,5 +14,26 @@ public class ArrayTools {
         }
 
         return  newArray;
+    }
+
+    /**
+     * @param firstArray
+     * @param secondArray
+     * @return return true, if arrays are equalent
+     */
+    public static boolean compare(int[] firstArray, int[] secondArray) {
+
+        if (firstArray.length != secondArray.length) {
+            return false;
+        }
+
+        for (int i = 0; i < firstArray.length; i++) {
+            if (Arrays.binarySearch(secondArray, firstArray[i]) == -1) {
+                return false;
+            }
+
+        }
+
+        return true;
     }
 }
