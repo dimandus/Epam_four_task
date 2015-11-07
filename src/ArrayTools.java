ae0de02231dbda52ee2f05c524cedc7d327d3bdc
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 13:58:08 +0300

    new change length function

--- src/ArrayTools.java
+++ src/ArrayTools.java
@@ -2,4 +2,15 @@
  * Created by Dimandus on 07.11.2015.
  */
 public class ArrayTools {
+
+    public static int[] changeLength(int[] array, int newLength){
+
+        int[] newArray = new int[newLength];
+
+        for(int i=0;i<newLength;i++){
+            newArray[i]=array[i];
+        }
+
+        return  newArray;
+    }
 }
