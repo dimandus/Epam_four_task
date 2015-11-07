a6c2359891fc5cbfc319c1004c45c3ac0b7da49c
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 14:32:32 +0300

    set print and isCorrect functions

--- src/Matrix.java
+++ src/Matrix.java
@@ -36,4 +36,42 @@ public class Matrix {
         items[row][column] = value;
     }
 
+    public String toString() {
+
+        String res = "";
+        if (!isCorrect(items)) {
+            return "array has nulls";
+        }
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items.length; j++) {
+                {
+                    res += items[i][j] + " ";
+                }
+            }
+            res += "\r\n";
+        }
+
+        return res;
+    }
+
+    /**
+     * ѕроверка, все ли значени€ матрицы инициализированы
+     * @param source матрица
+     * @return результат
+     */
+    public boolean isCorrect(double[][] source) {
+
+        if (source == null) {
+            return false;
+        }
+
+        boolean allOk = true;
+        for (int i = 0; i < source.length - 1; i++) {
+            if (source[i] == null) {
+                return false;
+            }
+        }
+
+        return allOk;
+    }
 }
