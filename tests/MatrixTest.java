878a3827c76ecc1f4e9475abea83f480728eee04
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 14:59:44 +0300

    new test directory and template tests for matrix

---
+++ tests/MatrixTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dimandus on 07.11.2015.
+ */
+public class MatrixTest {
+
+    @Test
+    public void testGetElementAt() throws Exception {
+
+    }
+
+    @Test
+    public void testSetElementAt() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testIsCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testAddition() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiplication() throws Exception {
+
+    }
+}
