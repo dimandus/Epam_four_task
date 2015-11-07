64ac6ec601b4186f8a699a225acd5c00afd229b4
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 16:25:02 +0300

    fix print array

--- src/ArrayTools.java
+++ src/ArrayTools.java
@@ -70,7 +70,13 @@ public class ArrayTools {
         String res = "";
 
         for (int i = 0; i < array.length; i++) {
-            res += array[i] + " ";
+            if(i!=array.length-1) {
+                res += array[i] + " ";
+            }
+            else
+            {
+                res+= array[i];
+            }
         }
         return res;
     }
