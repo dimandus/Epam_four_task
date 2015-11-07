b216dc28300f48e266b3d5b0703e0d0fd2ad585f
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-07 16:17:26 +0300

    new tests

--- tests/MatrixTest.java
+++ tests/MatrixTest.java
@@ -9,36 +9,41 @@ public class MatrixTest {
 
     @Test
     public void testGetElementAt() throws Exception {
-
+        assertEquals(new Matrix(new double[][]{{1, 2}, {3, 4}}).getElementAt(1, 0), 3, Double.MIN_VALUE);
     }
 
     @Test
     public void testSetElementAt() throws Exception {
-
-    }
-
-    @Test
-    public void testToString() throws Exception {
-
-    }
-
-    @Test
-    public void testIsCorrect() throws Exception {
-
-    }
-
-    @Test
-    public void testIsEqual() throws Exception {
-
+        Matrix matr = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        matr.setElementAt(1,0,6);
+        assertEquals(matr.getElementAt(1, 0), 6, Double.MIN_VALUE);
     }
 
     @Test
     public void testAddition() throws Exception {
+        Matrix matr1 = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        Matrix matr2 = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        assertTrue((Matrix.isEqual(Matrix.addition(matr1, matr2), new Matrix(new double[][]{{2, 4}, {6, 8}}))));
+    }
+
+    @Test
+    public void testMultiMatrNormal() throws Exception {
+        assertArrayEquals("Не умножаются верно", Matrix.multiplication(new Matrix(new double[][]{{1,2},{3,4}}), new Matrix(new double[][]{{1,2},{3,4}})).items,
+                new Matrix(new double[][]{{7,10},{15,22}}).items);
 
     }
 
     @Test
-    public void testMultiplication() throws Exception {
+    public void testMultiMatrDimensionsNotMatch() throws Exception {
+        assertNull("На выходе должен быть null",Matrix.multiplication(new Matrix(new double[][]{{1, 2}}), new Matrix(new double[][]{{1}})));
+
+    }
+
+    @Test
+    public void testMultiMatrOneOrMoreMatrixIsNull() throws Exception {
+        assertNull("На выходе должен быть null", Matrix.multiplication(new Matrix(null), new Matrix(new double[][]{{1, 2}, {1, 2}})));
+        assertNull("На выходе должен быть null", Matrix.multiplication(new Matrix(new double[][]{{1, 2}, {1, 2}}), null));
+        assertNull("На выходе должен быть null", Matrix.multiplication(null, null));
 
     }
 }
